3ff45c483bee0f455da9e63ed23403868336c4c0
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-11 23:52:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,27 @@
-import pandas as pd
+mport pandas as pd
 import numpy as np
+from scipy.stats import shapiro, ttest_ind
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1134491798 
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(X: np.ndarray, Y: np.ndarray, alpha: float) -> bool:
+    
+p1 = shapiro(X)
+p2 = shapiro(Y)
+if p1 < alpha or p2 < alpha:
+return False 
+p = fligner(X, Y)
+if p < alpha:
+equal_var = False 
+else:
+equal_var = True 
+
+
+p_value = ttest_ind(X, Y, equal_var=equal_var)
+
+
+if p_value < alpha:
+return True # Отклоняем нулевую гипотезу о равенстве средних значений
+else:
+return False # Не отклоняем нулевую гипотезу о равенстве средних значений
