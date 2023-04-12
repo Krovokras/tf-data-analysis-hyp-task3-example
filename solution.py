9ee5d7afdf02b62560379796b837be3b07cafe22
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-12 21:43:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,12 @@
-import scipy.stats as stats
+from scipy import stats
+from typing import List
+import pandas as pd
 import numpy as np
+
 chat_id = 1134491798 
-def solution(x: np.array, y: np.array) -> bool:
-    alpha = 0.03
-    _, p_value = stats.ks_2samp(x, y)
-    return p_value < alpha
+
+def solution(X: List[float], Y: List[float], alpha: float = 0.07) -> bool:
+    t_stat, p_value = stats.ttest_ind(Y, X, equal_var=False)
+    return p_value / 2 < alpha and t_stat > 0
+X = [500, 500, 500]
+Y = [500, 500, 525]
