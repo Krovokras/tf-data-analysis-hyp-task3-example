44e7f82faf14864e264ea87ccd2c165ca8b64599
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-12 21:39:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,7 @@
-import pandas as pd
+import scipy.stats as stats
 import numpy as np
-from scipy.stats import shapiro, ttest_ind
-
 chat_id = 1134491798 
-
-def solution(X: np.ndarray, Y: np.ndarray, alpha: float) -> bool:
-
-_, p1 = shapiro(X)
-_, p2 = shapiro(Y)
-if p1 < alpha or p2 < alpha:
-return False 
-_, p = fligner(X, Y)
-if p < alpha:
-    equal_var = False 
-else:
-    equal_var = True 
-_, p_value = ttest_ind(X, Y, equal_var=equal_var)
-
-
-if p_value < alpha:
-    return True 
-else:
-    return False 
+def solution(x: np.array, y: np.array) -> bool:
+    alpha = 0.03
+    _, p_value = stats.ks_2samp(x, y)
+    return p_value < alpha
