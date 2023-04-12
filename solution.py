d2602e4a298eeac399cf92edba31a7e7db46d3d9
Krovokras <130089605+Krovokras@users.noreply.github.com>
2023-04-12 21:19:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,24 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import ttest_ind
+from scipy.stats import shapiro, ttest_ind
+
+chat_id = 1134491798 
+
+def solution(X: np.ndarray, Y: np.ndarray, alpha: float) -> bool:
+
+_, p1 = shapiro(X)
+_, p2 = shapiro(Y)
+if p1 < alpha or p2 < alpha:
+return False 
+_, p = fligner(X, Y)
+if p < alpha:
+    equal_var = False 
+else:
+    equal_var = True 
+_, p_value = ttest_ind(X, Y, equal_var=equal_var)
 
 
-chat_id = 1134491798
-
-def solution(x: np.array, y: np.array, alpha: float) -> bool:
-   
-    is_normal_x = np.all(np.abs((x - np.mean(x)) / np.std(x)) < 3)
-    is_normal_y = np.all(np.abs((y - np.mean(y)) / np.std(y)) < 3)
-    
-   
-    is_homogeneous = np.abs(np.var(x) - np.var(y)) < 0.1*np.mean(np.abs([np.var(x), np.var(y)]))
-    
-    
-    if is_normal_x and is_normal_y and is_homogeneous:
-        t_statistic, p_value = ttest_ind(x, y, equal_var=True)
-        if p_value < alpha:
-            return True
-        else:
-            return False
-    else:
-        return False
+if p_value < alpha:
+    return True 
+else:
+    return False 
